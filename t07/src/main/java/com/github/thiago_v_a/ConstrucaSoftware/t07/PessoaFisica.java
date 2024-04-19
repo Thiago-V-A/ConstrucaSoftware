2afeb7f590e623c1cfce1450138c8aed1af6f0a8
Thiago-V-A <thiago234@discente.ufg.br>
2024-04-19 20:22:18 -0300

    endereco final

---
+++ t07/src/main/java/com/github/thiago_v_a/ConstrucaSoftware/t07/PessoaFisica.java
@@ -0,0 +1,10 @@
+package com.github.thiago_v_a.ConstrucaSoftware.t07;
+
+import java.util.Date;
+
+public class PessoaFisica {
+    private String nomePessoa;
+    private String sexo;
+    private Date dataNascimento;
+    private Endereco endereco;
+}
