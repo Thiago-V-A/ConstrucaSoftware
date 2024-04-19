13656419c00dddbbda988536dc8da00f3f4262e3
Thiago-V-A <thiago234@discente.ufg.br>
2024-04-19 20:06:01 -0300

    endereco1

---
+++ t07/src/main/java/com/github/thiago_v_a/ConstrucaSoftware/t07/Cidade.java
@@ -0,0 +1,6 @@
+package com.github.thiago_v_a.ConstrucaSoftware.t07;
+
+public class Cidade {
+    public String nomeCidade;
+    public Estado estado;
+}
---
+++ t07/src/main/java/com/github/thiago_v_a/ConstrucaSoftware/t07/Estado.java
@@ -0,0 +1,7 @@
+package com.github.thiago_v_a.ConstrucaSoftware.t07;
+
+public class Estado {
+    private String sigla;
+    public String nomeEstado;
+    private Pais pais;
+}
